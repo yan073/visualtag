c0faf77adcb218a474912a4f225bd5ba927757cc
Bo Yan <bo.yan@csiro.au>
2021-01-01 17:00:39 +1100

    refactor

--- protein_cath.js
+++ protein_cath.js
@@ -55,19 +55,6 @@ function set_color_to_cell(current, cat, leaves){
     }
 }
 
-function get_diff_color(adjcolors) {
-    if (adjcolors === undefined) {
-        return 1;
-    } else {
-        for(var i=1; i<=num_color_space; i++) {
-            if (adjcolors.indexOf(i) <0) {
-                return i;
-            }
-        }
-        return Math.floor(Math.random() * Math.floor(num_color_space)) + 1;
-    }
-}
-
 function get_cells_in_same_level2(level2, leaves){
     let samel = [];
     if (level2 != null && level2.length >0) {
@@ -127,27 +114,4 @@ function process_neighbor_after_coloring(current, color, level2, leaves){
         }
     }
 
-}
-
-function is_color_different(current, new_c, adjs){
-    for(var i=0; i< adjs.length; i++) {
-        if (adjs[i]  in colormap) {
-            if ( colormap[adjs[i]] == new_c ) return false;
-        }
-    }
-    return true;
-}
-
-function is_adjacent(rect1, rect2) {
-    if (is_same(rect1.top, rect2.bottom) || is_same(rect1.bottom, rect2.top)){
-        return rect1.left > rect2.right || rect1.right < rect2.left ? false : true;
-    }
-    if (is_same(rect1.left, rect2.right) || is_same(rect1.right, rect2.left)){
-        return rect1.top > rect2.bottom || rect1.bottom < rect2.top ? false : true;
-    }    
-    return false;
-}
-
-function is_same(x1, x2) {
-    return Math.abs(x1 - x2) < 5;  
 }
