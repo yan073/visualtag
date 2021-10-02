361afe5a36cfce3a8c7404014ca8587ad668e3d7
Bo Yan <bo.yan@csiro.au>
2021-10-02 12:06:44 +1000

    refactor

--- zenodo_util.js
+++ zenodo_util.js
@@ -1,8 +1,8 @@
-function query_zenodo_json_file(recid, filename, callback) {
-    d3.json("https://zenodo.org/api/records/" + recid, function(error, data1) {
-        d3.json(data1.links.latest, function(error, data2) {
+function query_zenodo_json_file(recid, filename, querier, callback) {
+    querier("https://zenodo.org/api/records/" + recid, function(error, data1) {
+        querier(data1.links.latest, function(error, data2) {
             fobj = data2.files.find(f => f.filename === filename);
-            d3.json(fobj.links.download, function(error, data3) {
+            querier(fobj.links.download, function(error, data3) {
                 callback(error, data3);
             });
         });
